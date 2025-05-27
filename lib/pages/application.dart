ba871d54eee35d046ddfeb45ddcf494073cdb29d
nbala123 <111918527+nbala123@users.noreply.github.com>
2025-05-26 21:03:12 -0500

    Implemented Rudimentary Grid

--- lib/pages/application.dart
+++ lib/pages/application.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:node_editor/node_editor.dart';
 
 class Application extends StatefulWidget {
   const Application({super.key});
@@ -8,17 +9,55 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
+  final NodeEditorController _controller = NodeEditorController();
+  final FocusNode _focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return FluentApp(
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Container(color: Colors.blue, width: 100, height: 50),
-            Container(color: Colors.red, width: 100, height: 50),
-          ],
-        ),
+      theme: FluentThemeData(menuColor: Colors.red),
+      home: LayoutBuilder(
+        builder: (context, constraints) {
+          final double maxSize =
+              (constraints.maxWidth > constraints.maxHeight)
+                  ? constraints.maxWidth
+                  : constraints.maxHeight;
+          final double canvasSize = maxSize * 2;
+
+          return Column(
+            children: [
+              Container(
+                color: const Color.fromARGB(255, 30, 30, 30),
+                child: MenuBar(
+                  items: [
+                    MenuBarItem(
+                      title: 'hello',
+                      items: [
+                        MenuFlyoutItem(
+                          text: Text('hello'),
+                          onPressed: () => {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: NodeEditor(
+                  controller: _controller,
+                  focusNode: _focusNode,
+                  background: const GridBackground(
+                    backgroundColor: Color.fromARGB(255, 20, 20, 20),
+                    lineColor: Color.fromARGB(255, 30, 30, 30),
+                    spacing: 40.0,
+                    strokeWidth: 2.0,
+                  ),
+                  infiniteCanvasSize: canvasSize,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
